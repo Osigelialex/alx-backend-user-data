c2d678becf9dcc65563520bb136fbc93084978ec
Osigelialex <osigelialex@gmail.com>
2023-08-03 22:23:29 +0100

    update

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 """password hashing module"""
 import bcrypt
-from typing import Union
 
 
-def hash_password(password: str) -> Union[bytes, None]:
+def hash_password(password: str) -> bytes:
     """returns hashed form of password"""
     salt = bcrypt.gensalt()
     hashed_password =  bcrypt.hashpw(password.encode('utf-8'), salt)
