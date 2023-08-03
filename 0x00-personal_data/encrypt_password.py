273d6b20528058b8028d111773b7188f98f991e0
Osigelialex <osigelialex@gmail.com>
2023-08-03 22:18:43 +0100

    update

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,6 +4,8 @@ import bcrypt
 from typing import Union
 
 
-def hash_password(password: str) -> bytes:
+def hash_password(password: str) -> Union[bytes, None]:
     """returns hashed form of password"""
+    if password is None or not isinstance(password, str):
+        return None
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
