26fd8b3b684f989edf667741cc10e1b22902c62a
Osigelialex <osigelialex@gmail.com>
2023-08-03 23:00:51 +0100

    task 1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -13,12 +13,15 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        NotImplementedError
-
+        log_message = super().format(record)
+        return filter_datum(self.fields, self.REDACTION,
+                            log_message, self.SEPARATOR)
+        
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
