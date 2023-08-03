e01909e933840611eb97130ba8e581c8f3832158
Osigelialex <osigelialex@gmail.com>
2023-08-03 21:16:37 +0100

    fix task 0

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,5 +6,23 @@ import re
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
-    pattern = fr'({re.escape(separator)}|^)({"|".join(re.escape(field) for field in fields)})=(.*?)(?={re.escape(separator)}|$)'
-    return re.sub(pattern, rf'\1\2={redaction}', message)
+    """
+    Obfuscates sensitive information in log messages
+    by replacing specific fields with a redaction string.
+
+    Args:
+        fields (List[str]): A list of strings 
+        representing the fields to be obfuscated.
+        redaction (str): A string representing the 
+        replacement for the obfuscated fields.
+        message (str): A string representing 
+        the log message to be obfuscated.
+        separator (str): A string representing the separator 
+        used in the log message to distinguish between fields.
+
+    Returns:
+        str: The obfuscated log message.
+    """
+    return re.sub(r'(\b{}=)[^{}]*'.format('|'.join(fields),
+                                          separator), r'\1' +
+                  redaction, message)
