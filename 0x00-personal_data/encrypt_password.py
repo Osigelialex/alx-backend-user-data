5f0df7861b8750cd964dc99f659eee8ef7cbb38d
Osigelialex <osigelialex@gmail.com>
2023-08-03 22:30:21 +0100

    pycodesytle fix

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -6,5 +6,10 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """returns hashed form of password"""
     salt = bcrypt.gensalt()
-    hashed_password =  bcrypt.hashpw(password.encode('utf-8'), salt)
+    hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """verifies if password is valid"""
+    return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
