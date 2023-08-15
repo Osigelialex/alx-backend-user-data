dd88273925c553b0a459d001b8e1a50cad942de6
Osigelialex <osigelialex@gmail.com>
2023-08-15 20:55:01 +0100

    add task 3

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
 from user import Base, User
 
 
@@ -16,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -37,3 +38,21 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, *args, **kwargs) -> User:
+        """finds user by input arguments
+        """
+        user = None
+
+        if len(kwargs) != 0:
+            user = self._session.query(User).filter_by(**kwargs).first()
+        else:
+            email = args[0]
+            password = args[1]
+            params = {"email": email, "hashed_password": password}
+            user = self._session.query(User).filter_by(**params).first()
+
+        if user is None:
+            raise NoResultFound
+
+        return user
