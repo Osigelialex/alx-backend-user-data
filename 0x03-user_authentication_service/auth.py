be98068f950f24e17370e138a4a22b7ed0bf50b8
Osigelialex <osigelialex@gmail.com>
2023-08-15 22:08:01 +0100

    updates

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""module for handling user authentication
+"""
+import bcrypt
+from db import DB
+from user import User
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a new user
+        """
+        if email is None or password is None:
+            return None
+
+        old = self._db._session.query(User).filter_by(email=email).first()
+        if old is not None:
+            raise ValueError(f'User {email} already exists')
+
+        hashed_password = _hash_password(password)
+        new_user = self._db.add_user(email, hashed_password)
+        return new_user
+
+
+def _hash_password(password: str) -> bytes:
+    """Returns the hashed form of a password
+    """
+    salt = bcrypt.gensalt()
+    hashed = bcrypt.hashpw(password.encode('utf-8'), salt)
+    return hashed
