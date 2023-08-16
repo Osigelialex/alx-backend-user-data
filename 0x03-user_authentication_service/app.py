aa89c9c076c540d09649524709afcacae4903270
Osigelialex <osigelialex@gmail.com>
2023-08-16 20:25:13 +0100

    added task 19

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -92,5 +92,25 @@ def get_reset_password_token() -> str:
         abort(403)
 
 
+@app.route("/reset_password", methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """updates the password of a user
+    """
+    email = request.form.get("email")
+    reset_token = request.form.get("reset_token")
+    new_password = request.form.get("new_password")
+
+    if reset_token is None or new_password is None or email is None:
+        abort(403)
+
+    try:
+        token = AUTH.get_reset_password_token(email)
+        AUTH.update_password(token, new_password)
+        payload = {"email": email, "message": "Password updated"}
+        return jsonify(payload), 200
+    except ValueError:
+        abort(403)
+
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
